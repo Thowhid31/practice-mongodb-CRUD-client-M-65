3a4bb61f3f929eb9a7a65a24703e26bf408616e5
Thowhid31 <thowhidantor16@gmail.com>
2022-04-27 00:11:30 +0600

    added last commit to kalboishakhi

--- src/App.js
+++ src/App.js
@@ -10,15 +10,42 @@ function App() {
     .then(data => setUsers(data))
   }, [])
 
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = {name, email};
+
+    //Post Data to server
+    fetch('', {
+      method:'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+
+  }
+
   return (
     <div className="App">
       <h1>My Own DATA: {users.length}</h1>
 
-      <p>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='name' required/>
+        <input type="text" name='email' placeholder='email' required/>
+        <input type="submit" value="Add User" />
+      </form>
+
+      <ul>
         {
           users.map(user=> <li key={user.id}>{user.id},{user.name}, {user.email}, {user.phone}</li>)
         }
-      </p>
+      </ul>
     </div>
   );
 }
