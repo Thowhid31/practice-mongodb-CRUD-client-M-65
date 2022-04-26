ff8ef197944d6f8cd021439ba6f223f7ac6ec919
Thowhid31 <thowhidantor16@gmail.com>
2022-04-26 23:51:20 +0600

    initial commit for practice client-site

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import AddUser from './Components/AddUser/AddUser';
-import Home from './Components/Home/Home';
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res =>res.json())
+    .then(data => setUsers(data))
+  }, [])
+
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='user/add' element={<AddUser></AddUser>}></Route>
-      </Routes>
+      <h1>My Own DATA: {users.length}</h1>
+
+      <p>
+        {
+          users.map(user=> <li key={user.id}>{user.id},{user.name}, {user.email}, {user.phone}</li>)
+        }
+      </p>
     </div>
   );
 }
