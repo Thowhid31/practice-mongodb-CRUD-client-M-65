27f4b04847b10a8e016762afcd65d0eef8e03391
Thowhid31 <thowhidantor16@gmail.com>
2022-04-27 23:18:09 +0600

    initial commit to first implement react router and others react function to start a project

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
 
-import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddUser from './Components/AddUser/AddUser';
+import Home from './Components/Home/Home';
 
 function App() {
-  const [users, setUsers] = useState([])
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='user/add' element={<AddUser></AddUser>}></Route>
+      </Routes>
+    </div>
+  );
+}
 
-  //load data
-  useEffect(() => {
-    fetch('http://localhost:5000/users')
-      .then(res => res.json())
-      .then(data => setUsers(data))
-  }, [])
-
-  const handleAddUser = event => {
-    event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const user = { name, email };
-
-    //Post Data to server
-    fetch('http://localhost:5000/user', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-      .then(res => res.json())
-      .then(data => {
-        const newUsers = [...users, data];
-        setUsers(newUsers);
-        console.log(data);
-      })
-    }
-
-    return (
-      <div className="App">
-        <h1>My Own DATA: {users.length}</h1>
-
-        <form onSubmit={handleAddUser}>
-          <input type="text" name='name' placeholder='name' required />
-          <input type="text" name='email' placeholder='email' required />
-          <input type="submit" value="Add User" />
-        </form>
-
-        <ul>
-          {
-            users.map(user => <li key={user.id}>{user.id}. {user.name}, {user.email}, {user.phone}</li>)
-          }
-        </ul>
-      </div>
-    );
-  }
-
-  export default App;
+export default App;
